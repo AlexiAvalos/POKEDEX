7abc5e588a1923f500944d22fffef568f81c9f69
Lee Sin <rp1065012023@edu.unab.sv>
2024-05-13 20:56:41 -0600

    Fixing Era

--- Pokedex/Pokedex/Main1.cs
+++ Pokedex/Pokedex/Main1.cs
@@ -1,22 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Pokedex
 {
     public partial class Main1 : Form
     {
+
         public Main1()
         {
             InitializeComponent();
+
         }
-<<<<<<< HEAD
+
 
 
         //no moverle que se rompe
@@ -30,34 +25,33 @@ namespace Pokedex
 
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
             VistaPokemon vistaPokemon = new VistaPokemon();
             vistaPokemon.Show();
             this.Hide();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)
         {
             VistaRegion vistaRegion = new VistaRegion();
             vistaRegion.Show();
             this.Hide();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void label3_Click(object sender, EventArgs e)
         {
             VistaObjetos vistaObjetos = new VistaObjetos();
             vistaObjetos.Show();
             this.Hide();
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void label4_Click(object sender, EventArgs e)
         {
             VistaTipo vistaTipo = new VistaTipo();
             vistaTipo.Show();
             this.Hide();
         }
-=======
->>>>>>> 2441536261f33169aaf0b473e683c6e3efe90d8e
     }
 }
+
